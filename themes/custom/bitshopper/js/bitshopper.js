5e505d85f295d4dd37c65032f09d90168186244c
Cameron <robinson.cameron90gmail.com>
2018-11-19 07:23:45 -0500

    further work on logo class

--- themes/custom/bitshopper/js/bitshopper.js
+++ themes/custom/bitshopper/js/bitshopper.js
@@ -1,27 +1,20 @@
-(function ($) {
-// $("div.input-group").addClass(classname, "single wow fadeInRight animated");
-const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+(function($) {
+  // $("div.input-group").addClass(classname, "single wow fadeInRight animated");
+  const viewportWidth =
+    window.innerWidth || document.documentElement.clientWidth;
+  function OnResizePull() {
+    $(document).ready(function() {
+      if (viewportWidth < 767) {
+        $(".logo").removeClass("pull-left");
+        console.log("small");
+      } else if (viewportWidth > 767) {
+        if (!$(".logo").hasClass("pull-left")) {
+          $(".logo").addClass("pull-left");
+          console.log("large");
+        }
+      }
+    });
+  }
 
-$(document).ready(function() {
-
-  if (viewportWidth < 767) {
-    $(".logo").removeClass("pull-left");
-    console.log("small");
-  }
-  else {
-    $(".logo").addClass("pull-left");
-    console.log("large");
-  }
-});
-
-$(window).resize(function(){
-  if (viewportWidth < 767) {
-    $(".logo").toggleClass("pull-left",addOrRemove);
-    console.log("small");
-  }
-  else if (viewportWidth > 767) {
-    $(".logo").toggleClass("pull-left",addOrRemove);
-    console.log("large");
-  }
-});
-}(jQuery));
+  $(window).on("resize", OnResizePull);
+})(jQuery);
