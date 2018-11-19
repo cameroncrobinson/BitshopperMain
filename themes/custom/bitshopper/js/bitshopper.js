16da217c041c38eb8a10e471e2d1e112a2f83d5e
Cameron <robinson.cameron90gmail.com>
2018-11-18 20:22:39 -0500

    navbar

--- themes/custom/bitshopper/js/bitshopper.js
+++ themes/custom/bitshopper/js/bitshopper.js
@@ -1,1 +1,27 @@
-$('div.input-group').addClass(classname,'single wow fadeInRight animated');
+(function ($) {
+// $("div.input-group").addClass(classname, "single wow fadeInRight animated");
+const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+$(document).ready(function() {
+
+  if (viewportWidth < 767) {
+    $(".logo").removeClass("pull-left");
+    console.log("small");
+  }
+  else {
+    $(".logo").addClass("pull-left");
+    console.log("large");
+  }
+});
+
+$(window).resize(function(){
+  if (viewportWidth < 767) {
+    $(".logo").toggleClass("pull-left",addOrRemove);
+    console.log("small");
+  }
+  else if (viewportWidth > 767) {
+    $(".logo").toggleClass("pull-left",addOrRemove);
+    console.log("large");
+  }
+});
+}(jQuery));
